03fbe5a6c4fed72813d726d33975cb9aa9abcb3d
noreply <noreply>
2020-02-08 16:58:29 -0500

    Setup simple setup/teardown for testing

--- Auto_Financial_Reporting_1.py
+++ Auto_Financial_Reporting_1.py
@@ -1,59 +1,63 @@
 import os
 import openpyxl
+from openpyxl import Workbook
 from openpyxl.utils import column_index_from_string as get_col_idx
 
 # os.chdir(os.getcwd() + r"\test excel files")  # Change working directory to folder with the excel files
 os.chdir(os.getcwd() + "/local_test")  # Change working directory to folder with the excel files
 
 # Set up - Import files (Identify main XL and 3 other XL to copy from)
-copy_wb = openpyxl.load_workbook(filename='CopyFrom.xlsx')
+copy_wb = openpyxl.load_workbook(filename='CopyFrom.xlsx', data_only=True)
 paste_wb = openpyxl.load_workbook(filename='PasteTo.xlsx', data_only=True)  # 'data_only' makes sure you only get data, Not formula in the cells
-result_wb = openpyxl.load_workbook(filename='result.xlsx', data_only=True)  # WB For Testing (Test results are saved here, so it doesn't overwrite original paste_wb's values in case of a mistake)
 
 copy_ws = copy_wb['Sheet1']
 paste_ws = paste_wb['Sheet1']
-result_ws = result_wb['Sheet1']
 
 
-def copy_paste_cell(copy_loc, paste_loc):
+def copy_paste_cell(copy_ws, paste_ws, copy_loc, paste_loc):
     """ Hardcoded Copy & Paste -- 'B2' -> 'B2' in other WS """
     paste_ws[paste_loc].value = copy_ws[copy_loc].value
-    paste_wb.save('result.xlsx')
 
-def copy_paste_range(copy_range, paste_range):
+def copy_paste_range(copy_ws, paste_ws, copy_range, paste_range):
     """ Hardcoded Copy & Paste -- "B2:B4" -> "B2:B4" in other WS """
-
     paste_ws["B2"].value = copy_ws["B2"].value
-    paste_wb.save('result.xlsx')
 
 # ----- Test -----
+test_wb = Workbook()
 
 # copy_paste_cell()
-# Test: Cell is empty before func. After Func, cell is copy/pasted correctly
-copy_loc = "B2"
-paste_loc = "B2"
-assert paste_ws[paste_loc].value != copy_ws[copy_loc].value
-copy_paste_cell(copy_loc, paste_loc)
-assert paste_ws[paste_loc].value == copy_ws[copy_loc].value
-assert result_ws[paste_loc].value == copy_ws[copy_loc].value
+test_ws = test_wb.create_sheet()
+copy_loc, paste_loc = "B2", "B2"
+assert test_ws[paste_loc].value != copy_ws[copy_loc].value
+copy_paste_cell(copy_ws, test_ws, copy_loc, paste_loc)
+assert test_ws[paste_loc].value == copy_ws[copy_loc].value
 
 
 # copy_paste_range()
-# Test: Cell range is empty before func. After Func, cell range is copy/pasted correctly
-copy_range = "B2:B4"
-paste_range = "B2:B4"
+test_ws = test_wb.create_sheet()
+copy_range, paste_range = "B2:B4", "B2:B4"
 # for row in range(3):
-#     assert paste_ws.cell(row=row+1, column=1).value != copy_ws.cell(row=row+1, column=1).value
+#     print(paste_ws.cell(row=row+2, column=2).value)
+#     print(copy_ws.cell(row=row+2, column=2).value)
+    # assert paste_ws.cell(row=row+2, column=2).value != copy_ws.cell(row=row+2, column=2).value
 # copy_paste_range(copy_loc, paste_loc)
 # for row in range(3):
 #     assert paste_ws.cell(row=row+1, column=1).value == copy_ws.cell(row=row+1, column=1).value
 
 
 
+def clear_cells(ws, loc):
+    """ Clear specified cell range of a ws """
+    for row in ws[loc]:
+        for cell in row:
+            cell.value = None
 
-
-
-
+# TEST clear_cells()
+test_paste_ws = test_wb.create_sheet()
+for row in test_paste_ws["A1:C3"]:
+    for cell in row:
+        cell.value = "1"
+assert test_paste_ws["A1:C3"] != None
 
 
 
